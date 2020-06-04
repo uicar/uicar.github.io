2007ff3df8abd1664df91ed16d25b2cc973040c5
David L Barrero <davidlopezbarrero@gmail.com>
2020-06-04 13:06:12 +0200

    Old websites

---
+++ Old/uicar web 1.0/assets/js/scripts.js
@@ -0,0 +1,8 @@
+// Add Your Scripts here
+
+// $(document).ready(function(){
+// 	$('.download-wrapper li').mouseenter(function(){
+// 		$('.download-wrapper li a').hide("slow");
+// 		$('.download-wrapper li a.extra').show("slow");
+// 	});
+// })
