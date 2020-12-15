18455cec0ecf3ad1e4e9041341ecb4b3a6c033ad
David L Barrero <davidlopezbarrero@gmail.com>
2020-12-15 15:18:23 +0100

    Cleaning

--- Old/uicar web 1.0/assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Add Your Scripts here
-
-// $(document).ready(function(){
-// 	$('.download-wrapper li').mouseenter(function(){
-// 		$('.download-wrapper li a').hide("slow");
-// 		$('.download-wrapper li a.extra').show("slow");
-// 	});
-// })
